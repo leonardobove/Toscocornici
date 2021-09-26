131e6eb3b8ae9b20acaeed77a2eeb0856171ac14
Leonardo Bove <leonardo.bove01@gmail.com>
2021-09-26 21:24:20 +0200

    Changes on laptop

--- toscocornici_app/lib/main.dart
+++ toscocornici_app/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:toscocornici_app/ui/authentication.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,27 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-  
-    return Scaffold(
-      body: Container(),
+      title: 'Toscocornici',
+      home: Authentication(),
     );
   }
 }
--- toscocornici_app/lib/ui/authentication.dart
+++ toscocornici_app/lib/ui/authentication.dart
@@ -1,2 +1,49 @@
 import 'package:flutter/material.dart';
 
+class Authentication extends StatefulWidget {
+  Authentication({Key? key}) : super(key: key);
+
+  @override
+  _AuthenticationState createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  TextEditingController _emailField = TextEditingController();
+  TextEditingController _passwordField = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          color: Colors.blueAccent,
+        ),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _emailField,
+              decoration: const InputDecoration(
+                hintText: "something@email.com",
+                hintStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                labelText: "Email",
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                )
+              )
+            ),
+            TextFormField(
+              controller: _passwordField,
+              obscureText: true,
+            ),
+            Container(),
+            Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
